d0d33da12d2e27d67a10f7f946b2ece88989fcad
Dan Cher <cher@wustl.edu>
2025-03-27 11:29:19 -0500

    :memo: better module names

--- data/eBird/run.py
+++ data/eBird/run.py
@@ -2,12 +2,12 @@ import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 
 from config import config
-import filter_data
-import combine_summarize
+import filter_eBird
+import combine_summarize_eBird
 
 # The data is first filtered down to the bounding box and time frame specified in the config.
 print("Running data filtering...")
-filter_data.main() 
+filter_eBird.main() 
 
 # Processing is done in R using the auk package which is regularly updated and made specifically for use with eBird data.
 print("Running checklist aggregation...")
@@ -17,4 +17,4 @@ process_ebird(config.beginning_date, config.ending_date, config.output_path)
 
 # Combine, summarize and clean up
 print("Combining, summarization and cleaning...")
-combine_summarize.main() 
+combine_summarize_eBird.main() 
